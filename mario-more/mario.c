d44bac264adc87fe5ea228195ee517aaab69f970
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-01-20 01:25:26 +0000

    Thu, Jan 20, 2022, 1:25 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -20,7 +20,7 @@ int j;
         {
             printf(".");
         }
-        for (j = 0; j > height - i; j--)
+        for (j = 0; j < 2*i - 1; j++)
         {
             printf("#");
         }
