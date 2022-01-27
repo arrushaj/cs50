6bbbe0d577566be1d02404e2f0adbbc6bfdb7090
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-01-27 03:44:15 +0000

    Thu, Jan 27, 2022, 3:44 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,8 +5,27 @@ int count_letters(string text)
 
 int main(int argc, string argv[])
 {
-    string a = get_string("Text: ");
-    printf("%s\n", a);
+    string text1 = get_string("Text: ");
+    printf("%s\n", text1);
 }
 
 int count_letters(string text)
+{
+    int total_letters = 0;
+    for (int i = 0, n = strlen(text); i < n, i++)
+    {
+        if isupper(text[i])
+        {
+            total_letters = total_letters + 1;
+        }
+        if islower(text[i])
+        {
+            total_letters = total_letters + 1;
+        }
+        else
+        {
+            total_letters = total_letters + 0;
+        }
+    }
+
+}
