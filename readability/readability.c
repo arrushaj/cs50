df4f9ac461759f5121f31b57fd1a54331fb30280
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-01-27 03:50:13 +0000

    Thu, Jan 27, 2022, 3:50 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,18 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int count_letters(string text)
+int count_letters(string text);
 
 int main(int argc, string argv[])
 {
     string text1 = get_string("Text: ");
     printf("%s\n", text1);
+    int total_letters = count_letters(text1);
+    printf("Total letters: %i\n", total_letters);
 }
 
 int count_letters(string text)
 {
     int total_letters = 0;
-    for (int i = 0, n = strlen(text); i < n, i++)
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
         if isupper(text[i])
         {
@@ -27,5 +31,5 @@ int count_letters(string text)
             total_letters = total_letters + 0;
         }
     }
-
+    return total_letters;
 }
