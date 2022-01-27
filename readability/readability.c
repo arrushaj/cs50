50839d16decc90afafe7e51edd52441ba3be123a
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-01-27 04:33:10 +0000

    Thu, Jan 27, 2022, 4:33 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -18,7 +18,7 @@ int main(int argc, string argv[])
     printf("%i letters\n", total_letters);
     printf("%i words\n", total_words);
     printf("%i sentences\n", total_sentences);
-    double index = 0.0588 * ((double) total_letters / (double) total_words) * 100) - 0.296 * ((double) total_sentences / (double) total_words) * 100) - 15.8;   // index = 0.0588 * L - 0.296 * S - 15.8
+    double index = (0.0588 * ((double) total_letters / (double) total_words) * 100) - 0.296 * ((double) total_sentences / (double) total_words) * 100) - 15.8);   // index = 0.0588 * L - 0.296 * S - 15.8
     int round_index = round(index);
     if (index < 1)
     {
