8f9aa33937043088e1ebdf00ade11935072c2f44
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-01-28 01:05:01 +0000

    Fri, Jan 28, 2022, 1:05 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,9 +3,17 @@
 
 int main(int argc, string argv[])
 {
-    if (argc != 1)  // If no input
+    if (argc != 2)  // If no input
     {
          printf("missing command-line argument\n");
          return 1;
+    }
+    if (argc == 2)
+    {
+        if (strlen(argv)) < 26
+        {
+        printf("Not enough letters in key\n");
+        }
+        if (strlen(argv)) = 26
     }
 }
