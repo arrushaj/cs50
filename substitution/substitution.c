e7d8c00f1355601f39e16a870eaf194660ed9bef
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-01-28 01:17:51 +0000

    Fri, Jan 28, 2022, 1:17 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -7,17 +7,17 @@ int main(int argc, string argv[])
 {
     if (argc != 2)  // If no input
     {
-         printf("missing command-line argument\n");
+         printf("Missing command-line argument\n");
          return 1;
     }
     if (argc == 2)
     {
-        if (strlen(argv) != 26)
+        if (strlen(argv[1]) != 26)
         {
         printf("Key must contain 26 characters.\n");
         return 1;
         }
-        if (strlen(argv) == 26)
+        if (strlen(argv[1]) == 26)
         {
         printf("Nice!\n");
         return 0;
