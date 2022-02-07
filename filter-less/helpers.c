24e9ce311252144371f2c305d19061bf852f403e
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-07 23:15:07 +0000

    Mon, Feb 7, 2022, 11:15 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    float average;
+    for (int i = 0; i < height - 1; i++)
+    {
+        for (int j = 0; j < width - 1; j++)
+        {
+            float average = (RGB)
+        }
+    }
     return;
 }
 
