5923d3ac2cde1d0d81d11a6bb9da133bea05d870
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-08 00:27:24 +0000

    Tue, Feb 8, 2022, 12:27 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -79,5 +79,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (i = 0)
+            {
+                if (j = 0)
+            }
+        }
+    }
     return;
 }
