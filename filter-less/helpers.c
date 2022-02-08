ab1964c60deef6f18bcec35b483d92747da09a0c
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-08 23:15:48 +0000

    Tue, Feb 8, 2022, 11:15 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -160,9 +160,20 @@ RGBTRIPLE copy[height - 1][width - 1];
                 image[i][j].rgbtGreen = round(averageGreen);
                 image[i][j].rgbtBlue = round(averageBlue);
             }
-            if (i == height - 1 && j != 0 && j != width - 1)
+            if (i == height - 1 && j != 0 && j != width - 1)       // BOTTOM EDGE
             {
-                
+                averageRed = (copy[i][j].rgbtRed + copy[i - 1][j].rgbtRed + copy[i][j - 1].rgbtRed + copy[i][j + 1].rgbtRed + copy[i - 1][j - 1].rgbtRed + copy[i - 1][j + 1].rgbtRed) / 6.0;
+                averageGreen = (copy[i][j].rgbtGreen + copy[i - 1][j].rgbtGreen + copy[i][j - 1].rgbtGreen + copy[i][j + 1].rgbtGreen + copy[i - 1][j - 1].rgbtRed + copy[i - 1][j + 1].rgbtRed) / 6.0;
+                averageBlue = (copy[i][j].rgbtBlue + copy[i - 1][j].rgbtBlue + copy[i][j - 1].rgbtBlue + copy[i][j + 1].rgbtBlue + copy[i - 1][j - 1].rgbtRed + copy[i - 1][j + 1].rgbtRed) / 6.0;
+                image[i][j].rgbtRed = round(averageRed);
+                image[i][j].rgbtGreen = round(averageGreen);
+                image[i][j].rgbtBlue = round(averageBlue);
+            }
+            else
+            {
+                averageRed = (copy[i][j].rgbtRed + copy[i - 1][j].rgbtRed + copy[i][j - 1].rgbtRed + copy[i][j + 1].rgbtRed + copy[i - 1][j - 1].rgbtRed + copy[i - 1][j + 1].rgbtRed) / 6.0;
+                averageGreen = (copy[i][j].rgbtGreen + copy[i - 1][j].rgbtGreen + copy[i][j - 1].rgbtGreen + copy[i][j + 1].rgbtGreen + copy[i - 1][j - 1].rgbtRed + copy[i - 1][j + 1].rgbtRed) / 6.0;
+                averageBlue = (copy[i][j].rgbtBlue + copy[i - 1][j].rgbtBlue + copy[i][j - 1].rgbtBlue + copy[i][j + 1].rgbtBlue + copy[i - 1][j - 1].rgbtRed + copy[i - 1][j + 1].rgbtRed) / 6.0;
             }
         }
     }
