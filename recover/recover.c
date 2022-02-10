0cca71ce7fc8570a2e56b4a7edbf76ea1c99f0fb
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-10 23:06:24 +0000

    Thu, Feb 10, 2022, 11:06 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
             }
             else if (i > 1)
             {
-                fclose(*img);
+                fclose(img);
             }
         }
     }
