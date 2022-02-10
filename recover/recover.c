af62bd1244dbe272449e4ed8a5be549550db3885
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-10 23:24:13 +0000

    Thu, Feb 10, 2022, 11:24 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     BYTE jpeg[BLOCK_SIZE];
 
     FILE *file = fopen(argv[1], "r");
-    int i = 1;
+    int i = 0;
     char filename[8];
     FILE *img;
 
@@ -18,14 +18,14 @@ int main(int argc, char *argv[])
     {
         if (jpeg[0] == 0xff & jpeg[1] == 0xd8 & jpeg[2] == 0xff & (jpeg[3] & 0xf0) == 0xe0)
         {
-            if (i == 1)
+            if (i == 0)
             {
                 sprintf(filename, "%03i.jpg", i);
                 img = fopen(filename, "w");
                 fwrite(jpeg, 1, BLOCK_SIZE, img);
                 i++;
             }
-            else if (i > 1)
+            else if (i > 0)
             {
                 fclose(img);
                 sprintf(filename, "%03i.jpg", i);
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
         }
         else
         {
-            if (i > 1)
+            if (i > 0)
             {
                 fwrite(jpeg, 1, BLOCK_SIZE, img);
             }
