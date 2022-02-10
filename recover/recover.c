8979626c1353fdc588b692af8db44f5732b43a45
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-10 22:55:43 +0000

    Thu, Feb 10, 2022, 10:55 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -15,15 +15,15 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] && 0xf0) == 0xe0)
         {
-            string filename;
-            sprintf(filename, "%03i.jpg", i);
-            if (i > 1)
+            if (i = 1)
             {
-                fclose(img);
+                string filename;
+                sprintf(filename, "%03i.jpg", i);
+                FILE *img = fopen(filename, "w");
+                fwrite(jpeg, BYTE, BLOCK_SIZE, img);
+                i++;
             }
-            FILE *img = fopen(filename, "w");
-            fwrite(jpeg, BYTE, BLOCK_SIZE, img);
-            i++;
+            else if (i > 1)
         }
     }
 
