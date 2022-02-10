e4625fd8fa410a05b2e6e3ab0146974c75460e16
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-10 22:09:20 +0000

    Thu, Feb 10, 2022, 10:09 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
     typedef uint8_t BYTE;
-    FILE* card = fopen(argv[1], "r");
+    FILE *file = fopen(argv[1], "r");
 
-    while(fread())
+    while (fread(buffer, 1, 512, raw_file) == 512)
     {
 
     }
