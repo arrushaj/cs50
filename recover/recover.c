cbcbdfffbf9e2bf54d0bfb13071dff572b5145fb
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-10 22:39:04 +0000

    Thu, Feb 10, 2022, 10:39 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,10 +14,10 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] && 0xf0) == 0xe0)
         {
-            string filename;
+            char *filename;
             sprintf(filename, "%03i.jpg", i);
             i++;
-            
+
         }
     }
 
