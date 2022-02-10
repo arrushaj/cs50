dabed3b0710290d4fbe07c9279a492231bb2fffc
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-10 22:43:57 +0000

    Thu, Feb 10, 2022, 10:43 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,10 +15,10 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] && 0xf0) == 0xe0)
         {
-            char *filename;
+            string filename;
             sprintf(filename, "%03i.jpg", i);
             i++;
-
+            
         }
     }
 
