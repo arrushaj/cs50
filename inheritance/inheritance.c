d7daf8347b25fd582ece2c92480bf37f0d9d12ec
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-14 23:39:27 +0000

    Mon, Feb 14, 2022, 11:39 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -41,6 +41,10 @@ person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
     person *p = malloc(sizeof(person));
+    if (p == NULL)
+    {
+        return 1;
+    }
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -52,7 +56,8 @@ person *create_family(int generations)
         p->parents[0] == parent0;
         p->parents[1] == parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-
+        p->alleles[0] == parent0->alleles[rand() % 2];
+        p->alleles[1] == parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
