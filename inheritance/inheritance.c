79c88f40b467b6046f2170fd0e8ade87811971f8
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-14 23:47:08 +0000

    Mon, Feb 14, 2022, 11:47 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -41,10 +41,6 @@ person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
     person *p = malloc(sizeof(person));
-    if (p == NULL)
-    {
-        return 1;
-    }
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -53,22 +49,22 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parents[0] == parent0;
-        p->parents[1] == parent1;
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] == parent0->alleles[rand() % 2];
-        p->alleles[1] == parent1->alleles[rand() % 2];
+        p->alleles[0] = parent0->alleles[rand() % 2];
+        p->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parents[0] == NULL
-        p->parents[1] == NULL
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
         // TODO: Randomly assign alleles
-        p->alleles[0] == random_allele();
-        p->alleles[1] == random_allele();
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
