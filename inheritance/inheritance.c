ded9c888c26149a4a04af73d08c362663e220206
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-14 23:51:16 +0000

    Mon, Feb 14, 2022, 11:51 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,8 +49,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->*parents[0] = parent0;
-        p->*parents[1] = parent1;
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         p->alleles[0] = parent0->alleles[rand() % 2];
         p->alleles[1] = parent1->alleles[rand() % 2];
@@ -80,8 +80,8 @@ void free_family(person *p)
         return;
     }
     // TODO: Free parents recursively
-    free_family(p->parent[0]);
-    free_family(p->parent[1]);
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
     // TODO: Free child
     free_family(p);
 }
