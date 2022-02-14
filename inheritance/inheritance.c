493f4e3f49d5502567aab86a3704ab20aa86bc9f
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-14 23:43:16 +0000

    Mon, Feb 14, 2022, 11:43 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -79,11 +79,15 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return;
+    }
     // TODO: Free parents recursively
-
+    free_family(p->parent[0]);
+    free_family(p->parent[1]);
     // TODO: Free child
-
+    free_family(p);
 }
 
 // Print each family member and their alleles.
