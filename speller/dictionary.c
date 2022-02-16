6c8657c5c9ed72dcdca697e2fb3c4e47aa3721c8
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-16 00:14:32 +0000

    Wed, Feb 16, 2022, 12:14 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -17,7 +17,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = (26^2);      // Buckets for first 2 letters (aa, ab, ac, etc)
+const unsigned int N = 250;
 
 // Hash table
 node *table[N];
