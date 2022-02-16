b07cc1fd833d39fe53dec948f159bc9eb6f48fb0
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-16 01:41:58 +0000

    Wed, Feb 16, 2022, 1:41 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -86,7 +86,7 @@ bool load(const char *dictionary)
         }
         strcpy(n->word, buffer);
         int index = hash(n->word);
-        n = table[index];
+        n->next = table[index];
         table[index] = n;
     }
     return true;
