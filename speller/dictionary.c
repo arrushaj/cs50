1ea18d85b59f80222ccde40b450fd652fceabc51
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-16 00:32:33 +0000

    Wed, Feb 16, 2022, 12:32 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -79,7 +79,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-   if load()
+   if !loaded
     return 0;
 }
 
