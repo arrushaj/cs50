fc69bc7a2f64de32c677e17ad0af121f9ed82654
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-16 01:39:01 +0000

    Wed, Feb 16, 2022, 1:39 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -61,7 +61,7 @@ unsigned int hash(const char *word)
         index += (toupper(word[i]) - 'A');
     }
     // TODO: Improve this hash function
-    if (index > N - 1)
+    if (index > N)
     {
         index = index % N;
     }
