d1773973c3ed88ba8c3b32ee16a13bee7bdc785f
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-16 01:06:45 +0000

    Wed, Feb 16, 2022, 1:06 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -25,7 +25,9 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    int index = hash(word);
+    node *n = malloc(sizeof(node));
+    n = table[index];
     return false;
 }
 
