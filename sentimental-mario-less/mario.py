b3bbceeb6ff7e57c0892e5684044aa63e055050b
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-25 19:15:51 +0000

    Fri, Feb 25, 2022, 7:15 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -6,6 +6,6 @@ while True:
         break
 
 for i in range(n):
-        print(" " * (n-i-1), end="")
-        print("#" * (i+1), end="\n")
+    print(" " * (n-i-1), end="")
+    print("#" * (i+1), end="\n")
 
