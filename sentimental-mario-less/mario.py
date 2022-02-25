133055b84dc23b2a275724ab586387beb7d6558b
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-25 19:10:53 +0000

    Fri, Feb 25, 2022, 7:10 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,12 +1,12 @@
 from cs50 import get_int
 
 while True:
-n = get_int("Height: ")
+    n = get_int("Height: ")
     if n > 0 or n < 9:
         break
 
-for i in range(n)
-    for j in range(n)
-        print(" " * ())
-        print("#" * (i+1))
+for i in range(n):
+    for j in range(n):
+        print(" " * (n-i-1), end="")
+        print("#" * (i+1), end="\n")
 
