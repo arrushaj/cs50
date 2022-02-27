14ff2f40e7aae34e859476c60386f6bc34f3f822
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-27 01:35:22 +0000

    Sun, Feb 27, 2022, 1:35 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,20 +7,18 @@ length = len(str(number))
 total = 0
 checksum = 0
 j = 1
-for i in range(length):
+for i in range(0, length, 2):
     place = 10 ** j
     digit = number % (place)
     print(digit)
     double = digit * 2
     total = total + double
     j += 2
-    i += 2
 
 j = 2
-for k in range(length):
+for k in range(1, length, 2):
     digit = number % (10^j)
     checksum = checksum + total + digit
     j = j + 2
-    k = k + 2
 
 print(checksum)
