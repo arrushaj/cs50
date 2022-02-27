41b7b7287a34ff52d0b9860f4cd6dcb8f47e86b9
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-27 02:19:51 +0000

    Sun, Feb 27, 2022, 2:19 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -9,12 +9,11 @@ checksum = 0
 for i in range(1, length, 2):
     digit = int(str(number)[length-i-1])
     digit = digit * 2
-    print(digit)
     total = total + digit
 
+print(total)
 for i in range(0, length, 2):
     digit = int(str(number)[length-i-1])
-    print(digit)
     checksum = checksum + total + digit
 
 print(checksum)
