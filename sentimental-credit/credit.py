07a237857fde1b6670b2db8d096b9b652729177d
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-27 01:29:56 +0000

    Sun, Feb 27, 2022, 1:29 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -8,7 +8,8 @@ total = 0
 checksum = 0
 j = 1
 for i in range(length):
-    digit = number % (10)
+    place = 10^j
+    digit = number % (place)
     print(digit)
     double = digit * 2
     total = total + double
