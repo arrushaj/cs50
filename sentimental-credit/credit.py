fa3647397be129d954e1307d7a01cbe14a083262
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-27 01:58:13 +0000

    Sun, Feb 27, 2022, 1:58 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -19,5 +19,5 @@ if checksum % 10 != 0:
     print("INVALID")
 
 if length == 15:
-        if str(number)[0] + str(number)(1) == 34 or 37:
+        if str(number)[0] + str(number)[1] == str(34) or str(37):
             print("AMEX")
