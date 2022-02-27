29f26f619fc1bcb56eff02e45e10fe95910e3d26
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-02-27 01:19:24 +0000

    Sun, Feb 27, 2022, 1:19 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,9 +2,9 @@ from cs50 import get_int
 
 number = get_int("Number: ")
 
+length = len(str(number))
 
-
-for i in range(len(number)):
+for i in range(length):
     j = 1
     digit = number % (10^j)
     double = digit * 2
@@ -12,7 +12,7 @@ for i in range(len(number)):
     j = j + 2
     i = i + 2
 
-for k in range(len(number)):
+for k in range(length):
     j = 2
     digit = number % (10^j)
     checksum = checksum + total + digit
