cc9df3cfa9c60c9a37b20d1839a9208d1c4e181d
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-03-01 02:47:24 +0000

    Tue, Mar 1, 2022, 2:47 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -16,5 +16,14 @@ for i in range(len(text)):
     elif lower[i] == "?" or "." or "!":
         sentences += 1
 
+index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
+if index < 1:
+    print("Before Grade 1")
+
+elif index > 16:
+    print("Grade 16+")
+
+else:
+    print("Grade " + round(index))
 
