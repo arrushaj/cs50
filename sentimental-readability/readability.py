0ad8b4f2daafa4325894df207d15d85830ea2e7a
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-03-01 02:43:30 +0000

    Tue, Mar 1, 2022, 2:43 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,7 +4,17 @@ text = get_string("Text: ")
 
 words = 0
 sentences = 0
+letters = 0
+
+lower = text.lower
 
 for i in range(len(text)):
-    if text[i].lower > ord(a) and
-    if text[i] = " ":
+    if ord(lower[i]) > ord('a') and ord(lower[i]) < ord('z'):
+        letters += 1
+    elif lower[i] == " ":
+        words += 1
+    elif lower[i] == "?" or "." or "!":
+        sentences += 1
+
+
+
