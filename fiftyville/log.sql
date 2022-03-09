1e7415684d1675c51419590a0bf7b6d285a4ffa0
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-03-09 23:57:03 +0000

    Wed, Mar 9, 2022, 11:57 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 and day = 28 and street = "Humphrey Street";
+-- We know the date of the crime and the street it took place on, now the logical thing to do is get a description of it. Description revealed three witnesses who were present and the bakery is mentioned.
 
