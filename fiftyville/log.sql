920edb3175ef97503b5b8e4586654475ce20e694
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-03-10 00:11:10 +0000

    Thu, Mar 10, 2022, 12:11 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,3 +10,17 @@ SELECT name, transcript FROM interviews WHERE year = 2021 AND month = 7 and day 
 -- Raymond reveals that as the thief was leaving the bakery, he talked to someone on the phone and he heard him say that he was planning on taking the earliest flight out of Fiftyville the next day
 -- Raymond also reveals that the thief asked the person on the line to purchase the flight tickets
 -- Emma, the bakery owner, heard someone whispering in the store for half an hour
+SELECT activity, license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25;
+-- Using Ruth's testimony, I looked up footage that took place within 10 minutes of the crime and it reveals (from 10:15 to 10:25) and it reveals these license plates exited the parking lot:
+--+----------+---------------+
+--| activity | license_plate |
+--+----------+---------------+
+--| exit     | 5P2BI95       |
+--| exit     | 94KL13X       |
+--| exit     | 6P58WS2       |
+--| exit     | 4328GD8       |
+--| exit     | G412CB7       |
+--| exit     | L93JTIZ       |
+--| exit     | 322W7JE       |
+--| exit     | 0NTHK55       |
+--+----------+---------------+
