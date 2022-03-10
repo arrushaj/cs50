09e9e1cecd59b2e657503abc139ac96e43665639
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-03-10 00:08:22 +0000

    Thu, Mar 10, 2022, 12:08 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,7 +5,7 @@ SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 and 
 -- Crime took place at 10:15am
 SELECT name, transcript FROM interviews WHERE year = 2021 AND month = 7 and day = 28;
 -- We use this query to get transcripts of interviews from the date of the crime.
--- Ruth reveals that the thief left the scene of the crime shortly afterwards in a car and that we can use footage to see which car it was.
+-- Ruth reveals that the thief left the scene of the crime shortly afterwards (within ten minutes of the theft) in a car and that we can use footage to see which car it was.
 -- Eugene reveals that he saw the thief withdrawing money from an ATM on Leggett Street
 -- Raymond reveals that as the thief was leaving the bakery, he talked to someone on the phone and he heard him say that he was planning on taking the earliest flight out of Fiftyville the next day
 -- Raymond also reveals that the thief asked the person on the line to purchase the flight tickets
