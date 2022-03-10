9e9bf8e24db1b081557c60266cc63110e13429b0
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-03-10 00:03:10 +0000

    Thu, Mar 10, 2022, 12:03 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 and day = 28 and street = "Humphrey Street";
--- We know the date of the crime and the street it took place on, now the logical thing to do is get a description of it. Description revealed three witnesses who were present and the bakery is mentioned.
+-- We know the date of the crime and the street it took place on, now the logical thing to do is get a description of it.
+-- Description revealed three witnesses who were present and the bakery is mentioned.
 SELECT name, transcript FROM interviews WHERE year = 2021 AND month = 7 and day = 28;
--- 
+-- We use this query to get transcripts of interviews from the date of the crime.
+-- Ruth reveals that the thief left the scene of the crime shortly afterwards in a car and that we can use footage to see which car it was.
+-- Raymond reveals 
