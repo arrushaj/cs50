5dc9aab092844585c60cdae8cd03260e5b888536
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-03-10 00:28:31 +0000

    Thu, Mar 10, 2022, 12:28 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -42,3 +42,5 @@ SELECT account_number, amount, transaction_type FROM atm_transactions WHERE year
 SELECT origin_airport_id, destination_airport_id, hour, minute FROM flights WHERE year = 2021 AND month = 7 AND day = 29 ORDER BY hour, minute;
 -- Using the above query, we were able to get the earliest flight that took place at 8:20 the next day (the 29th)
 -- The origin_airport_id is 8 and the destination_airport_id is 4
+ SELECT city FROM airports WHERE id = 4;
+-- Using the above query, we discover that the city the thief most likely fled to was New York City!!
