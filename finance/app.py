61b8a7e24dc0f8e75b313c6a6d8223c97e1250b8
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-04-01 00:00:29 +0000

    Fri, Apr 1, 2022, 12:00 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -111,6 +111,12 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
+    if request.method == "POST":
+        
+
+    else:
+        return render_template("quote.html")
+
     return apology("TODO")
 
 
