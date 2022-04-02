1e193a914008f797f0a1ec49fbd0d6d89988e66d
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-04-02 23:34:18 +0000

    Sat, Apr 2, 2022, 11:34 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -65,7 +65,7 @@ def buy():
         price = stock["price"]
         cash = db.execute("SELECT cash FROM users WHERE id = ?", id)
         cost = price * shares
-        print
+        print(cash)
         new_cash = cash - cost
 
         if new_cash < 0:
