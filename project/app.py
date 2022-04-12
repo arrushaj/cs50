772e258c130084855972ca647bcfa51a8b16f92b
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-04-12 22:49:46 +0000

    Tue, Apr 12, 2022, 10:49 PM +00:00

--- project/app.py
+++ project/app.py
@@ -7,6 +7,8 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import apology, login_required
+
 # Configure application
 app = Flask(__name__)
 
@@ -19,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///blablabla.db")
+#db = SQL("sqlite:///blablabla.db")
 
 @app.after_request
 def after_request(response):
