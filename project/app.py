164a9e01e2ceb628df5c428c6c40be04a454e99d
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-04-12 22:54:49 +0000

    Tue, Apr 12, 2022, 10:54 PM +00:00

--- project/app.py
+++ project/app.py
@@ -21,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-#db = SQL("sqlite:///blablabla.db")
+db = SQL("sqlite:///forum.db")
 
 @app.after_request
 def after_request(response):
