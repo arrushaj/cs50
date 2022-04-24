337503a17fbbec2663fd24338e34c4ba0df72017
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-04-24 22:43:38 +0000

    Sun, Apr 24, 2022, 10:43 PM +00:00

--- project/app.py
+++ project/app.py
@@ -64,7 +64,7 @@ def login():
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
-        current_user.is_authenticated = True
+        user.is_authenticated = True
 
         # Redirect user to home page
         return redirect("/")
