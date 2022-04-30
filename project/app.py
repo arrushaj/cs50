dcf5ad4164f36d0d0f7113344475c3f88cd17102
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-04-30 21:15:12 +0000

    Sat, Apr 30, 2022, 9:15 PM +00:00

--- project/app.py
+++ project/app.py
@@ -374,15 +374,15 @@ def reply_form():
     if request.method == "GET":
         id = request.args.get("reply_id")
 
-        message_row = db.execute("SELECT * FROM replies WHERE id = ?", id)
+        row = db.execute("SELECT * FROM replies WHERE id = ?", id)
 
-        thread_id = message_row[0]["thread_id"]
+        thread_id = row[0]["thread_id"]
 
         y = db.execute("SELECT * FROM replies WHERE id = ? AND thread_id = ?", id, thread_id)
 
         if len(y) < 1:
             return apology("That post is not in this thread!")
 
-        return render_template("reply.html", message_row=message_row)
+        return render_template("reply.html", row=row)
 
 
