12a781fc76f5fd63804140725bb4ad417524c20f
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-05-03 20:21:05 +0000

    Tue, May 3, 2022, 8:21 PM +00:00

--- project/app.py
+++ project/app.py
@@ -4,7 +4,6 @@ import datetime
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, Blueprint
 from flask_session import Session
-from flask-paginate import Pagination
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
