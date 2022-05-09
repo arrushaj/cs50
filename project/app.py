2fcf7fe8eca90f5d6f50000a30bd1d5e59dd8362
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-05-09 23:45:52 +0000

    Mon, May 9, 2022, 11:45 PM +00:00

--- project/app.py
+++ project/app.py
@@ -246,7 +246,7 @@ def reply():
         rows = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])
         username = rows[0]["username"]
 
-        db.execute("INSERT INTO replies (thread_id, user, message, date) VALUES (?, ?, ?, strftime('%Y/%m/%d %H:%M:%S'))", thread, username, message)
+        db.execute("INSERT INTO replies (thread_id, user, message, date, user_id) VALUES (?, ?, ?, strftime('%Y/%m/%d %H:%M:%S'), ?)", thread, username, message, session["user_id"])
         db.execute("UPDATE thread SET replies = replies + 1 WHERE id = ?", thread)
         db.execute("UPDATE thread SET latest = strftime('%Y/%m/%d %H:%M:%S') WHERE id = ?", thread)
 
