4cdb9013cf130359835ecade76ff2dae70dfd684
arrushaj <98062280+arrushaj@users.noreply.github.com>
2022-05-09 23:09:44 +0000

    Mon, May 9, 2022, 11:09 PM +00:00

--- project/app.py
+++ project/app.py
@@ -443,8 +443,9 @@ def reply_legit():
 @app.route("/profile")
 def profile():
     user = request.args.get("user")
-    user_id_get = db.execute("SELECT * FROM users WHERE username = ?", user)
-    user_id = user_id_get[0]["id"]
+    get = db.execute("SELECT * FROM users WHERE username = ?", user)
+    user_id = get[0]["id"]
+    bio = get[0]["bio"]
 
-
+    return render_template("profile.html", user, bio)
 
